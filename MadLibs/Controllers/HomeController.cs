563f508db5743d6922908faf4dc8796af3fd8f2e
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-14 14:21:05 -0800

    functional madlib generator
    Co-authored-by: Garrett Dean Brown <gman9mm@live.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,6 +5,39 @@ namespace MadLibs.Controllers
 {
     public class HomeController : Controller
     {
-      
+      [Route("/madlib")]
+      public ActionResult MadLib(string verb, string pluralnoun, string adjective, string food, string noun, string exclamation, string name, string verbreaction, string location) 
+      {
+        MadlibVariable madLib = new MadlibVariable(); 
+        madLib.Verb = verb;
+        madLib.PluralNoun = pluralnoun;
+        madLib.Adjective = adjective;
+        madLib.Food = food;
+        madLib.Noun = noun;
+        madLib.Exclamation = exclamation;
+        madLib.Name = name;
+        madLib.VerbReaction = verbreaction;
+        madLib.Location = location;
+        return View(madLib);
+      }
+      [Route("/madlibtwo")]
+      public ActionResult MadLibTwo(string verb, string pluralnoun, string adjective, string food, string noun, string exclamation, string name, string verbreaction, string location) 
+      {
+        MadlibVariable madLib = new MadlibVariable(); 
+        madLib.Verb = verb;
+        madLib.PluralNoun = pluralnoun;
+        madLib.Adjective = adjective;
+        madLib.Food = food;
+        madLib.Noun = noun;
+        madLib.Exclamation = exclamation;
+        madLib.Name = name;
+        madLib.VerbReaction = verbreaction;
+        madLib.Location = location;
+        return View(madLib);
+      }
+
+      [Route("/form")]
+      public ActionResult Form() { return View(); }
     }
+
 }
--- MadLibs/Models/MadLibVariables.cs
+++ MadLibs/Models/MadLibVariables.cs
@@ -2,6 +2,15 @@ namespace MadLibs.Models
 {
     public class MadlibVariable
     {
+      public string Verb { get; set; }
+      public string Adjective { get; set; }
+      public string Noun { get; set; }
+      public string Location { get; set; }
+      public string Exclamation { get; set; }
+      public string Name { get; set; }
+      public string PluralNoun { get; set; }
+      public string VerbReaction { get; set; }
+      public string Food { get; set; }
       
     }
 }
